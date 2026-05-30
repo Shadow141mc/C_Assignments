588a439960850e9b94ddd571182ac409150fbfd8
Mainak Chakraborty <94784728+Shadow141mc@users.noreply.github.com>
2026-05-30 19:56:11 +0530

    Create program8.c

---
+++ assignment5/program8.c
@@ -0,0 +1,26 @@
+/*
+Print the following pattern
+A
+BB
+CCC
+DDDD
+EEEEE
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    char c = 'A';
+    for(i = 1; i <=5; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%c", c);
+        }
+        c++;
+        printf("\n");
+    }
+    return 0;
+}
