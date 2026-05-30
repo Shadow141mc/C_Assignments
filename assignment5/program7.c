a49cb00b32f117fc4edca4bf65335550690b9a6b
Mainak Chakraborty <94784728+Shadow141mc@users.noreply.github.com>
2026-05-30 18:29:53 +0530

    Create program7.c

---
+++ assignment5/program7.c
@@ -0,0 +1,31 @@
+/*
+Print the following pattern
+55555
+45555
+34555
+23455
+12345
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int i,j,k;
+	for(i = 5; i >= 1; i--)
+	{
+		k = i;
+		for(j = 1; j <= 5; j++)
+		{
+			if(k <= 5)
+			{
+			    printf("%d", k);
+			    k++;
+			}
+			else
+			    printf("5");
+		}
+		printf("\n");
+	}
+	return 0;
+}
